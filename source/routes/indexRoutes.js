ef89fbd68d3476b4838173aa8389b8657148ba2f
Marlon Perini <marlon.perini@gmail.com>
2021-06-26 23:30:45 +0200

    commit missing files

--- source/routes/indexRoutes.js
+++
@@ -1,8 +0,0 @@
-import express from 'express';
-
-const router = express.Router();
-import {indexController} from '../controller/indexController.js';
-
-router.post("/login", indexController.login.bind(indexController));
-
-export const indexRoutes = router;
